444203fe6b0f5e52d5058126a4ca75cec1e35e7f
Diep Dao <diep.dao@thomsonreuters.com>
2020-12-25 19:08:37 +0800

    Add configs and download http file in chunks

---
+++ agoda/config/config.py
@@ -0,0 +1,23 @@
+import os
+
+import yaml
+
+from agoda.common.singleton import Singleton
+
+
+class Config(object, metaclass=Singleton):
+    def __init__(self, config_file=None):
+        if not config_file:
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            config_file = os.path.join(dir_path, 'config.yaml')
+        self._configs = self._load(config_file)
+
+    @staticmethod
+    def _load(config_file):
+        with open(config_file, 'r') as f:
+            configs = yaml.safe_load(f.read())
+
+        return configs
+
+    def get(self, name):
+        return self._configs.get(name)
--- agoda/downloader/http_downloader.py
+++ agoda/downloader/http_downloader.py
@@ -1,16 +1,21 @@
 import requests
 
+from agoda.config.config import Config
 from agoda.downloader import DownloaderIF, URL
 
 
 class HTTPDownloader(DownloaderIF):
+    def __init__(self):
+        self.configs = Config()
 
     def authenticate(self, url: URL) -> bool:
         pass
 
     def download(self, url, dest_path):
-        with open(dest_path, 'wb') as f:
-            r = requests.get(url, allow_redirects=True)
-            f.write(r.content)
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(dest_path, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=self.configs.get('downloader')['chunk_size']):
+                    f.write(chunk)
 
         return dest_path
